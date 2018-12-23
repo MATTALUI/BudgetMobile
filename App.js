7c25873b3144ef9c5a0ea389eb38d53358c214da
Matthew Hummer <biggreyson@gmail.com>
2018-12-22 23:41:29 -0800

    add some POC data/structures

--- App.js
+++ App.js
@@ -7,7 +7,25 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, ScrollView, View} from 'react-native';
+
+/*
+Budget:
+  [Income]
+  [Expense]
+  "name"
+  #datesaved
+  "type" (only if local v db loads)
+Income:
+  "name"
+  #value
+Expense:
+  "name"
+  #value/"value"
+Category << Income/Expense:
+  "category"
+  "color"
+*/
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,13 +39,21 @@ export default class App extends Component<Props> {
   constructor(props){
     super(props);
     //dummy data for now
+    let inc =[];
+    let exp =[];
+    for(let i =0; i<=(Math.random() *20);i++){
+      inc.push({name: "inc-"+i});
+      exp.push({name: "exp-"+i});
+    }
     this.state = {
       user: {
         id: 1,
         firstName: "Matthew",
         lastName: "Hummer",
         username: "mattalui"
-      }
+      },
+      incomes: inc,
+      expenses: exp
     };
   }
   render() {
@@ -40,6 +66,17 @@ export default class App extends Component<Props> {
       <View>
         <View style={[styles.navbar]}>
           <Text style={[styles.whiteText]}>Budget Calculator</Text>
+          <Text style={[styles.halfText]}>{name}</Text>
+        </View>
+        <ScrollView>
+          {this.state.incomes.map((inc, i)=>(
+            <Text key = {i} style={[styles.test]}>{inc.name}</Text>
+          ))}
+          {this.state.expenses.map((exp, i)=>(
+            <Text key = {i} style={[styles.test]}>{exp.name}</Text>
+          ))}
+        </ScrollView>
+        <View style={[styles.footer]}>
         </View>
       </View>
     );
@@ -49,10 +86,23 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   navbar: {
     backgroundColor: "#343a40",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  footer: {
+    backgroundColor: "#343140",
+    height: 10
   },
   whiteText: {
     color: "#ffffff",
     fontSize: 20
+  },
+  halfText: {
+    color: "rgba(255,255,255,.25)",
+    fontSize: 20
+  },
+  test: {
+    fontSize: 40
   }
 });
 
